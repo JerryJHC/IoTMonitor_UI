45ca13ae8dc06eace975b4f5b4bafe1665d2df00
Jerry Henriquez <jerryhenriquez@gmail.com>
2019-05-18 12:30:56 +0200

    ITM-25 Generar componentes
    ^ITM
    
    Creado component Charts, para contener los chart que se genenren para el usuario, se ha incorporado en el AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartsComponent } from './components/charts/charts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/charts/charts.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Chart } from "../../models/Chart"
+
+@Component({
+  selector: 'app-charts',
+  templateUrl: './charts.component.html',
+  styleUrls: ['./charts.component.css']
+})
+export class ChartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
