951a84a90cb1031ab928d914582cd47daca1f28b
Jerry Henriquez <jerryhenriquez@gmail.com>
2019-06-08 12:21:52 +0200

    ITM-38 Ultimas medidas
    ^ITM-38 Done
    
    Agregado botón que llama a la función getLatestMeasures para volver a solicitar los datos
    
    Se ha modificada la frase de ultima medida a actualizado
    
    Se ha modificado la generación del datetime para que los minutos aparezcan con un 0 si van de 1 a 9
    
    Las medidas ahora aparecen solo con 2 decimales

--- src/app/component/current-measure/current-measure.component.ts
+++ src/app/component/current-measure/current-measure.component.ts
@@ -25,24 +25,24 @@ export class CurrentMeasureComponent implements OnInit {
 
     //Request latest measures
     this.firebaseData.getLastTemperature().subscribe(temperature => {
-      this.currentMeasure.temperature = temperature[0].value;
+      this.currentMeasure.temperature = parseFloat(temperature[0].value.toFixed(2));
       this.setDateTime(new Date(temperature[0].datetime));
       this.progress++;
     });
     this.firebaseData.getLastHumidity().subscribe(humidity => {
-      this.currentMeasure.humidity = humidity[0].value;
+      this.currentMeasure.humidity = parseFloat(humidity[0].value.toFixed(2));
       this.progress++;
     });
     this.firebaseData.getLastPressure().subscribe(pressure => {
-      this.currentMeasure.pressure = pressure[0].value;
+      this.currentMeasure.pressure = parseFloat(pressure[0].value.toFixed(2));
       this.progress++;
     });
   }
 
   //Format Last Measure DateTime
   setDateTime(datetime: Date) {
-    console.log(datetime);
-    this.currentMeasure.datetime = '' + datetime.getDate() + '/' + (datetime.getMonth() +1) + '/' + datetime.getUTCFullYear() + ' ' + datetime.getHours() + ':' + datetime.getHours();
+    let minutes = datetime.getMinutes() < 10 ? '0' + datetime.getMinutes() : datetime.getMinutes();
+    this.currentMeasure.datetime = '' + datetime.getDate() + '/' + (datetime.getMonth() + 1) + '/' + datetime.getUTCFullYear() + ' ' + datetime.getHours() + ':' + minutes;
   }
 
   //Set Dynamic classes
